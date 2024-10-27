cb0cea8c63afabc01dc88b319af71a6f21f02ab9
Robert <robert.burlacu03@gmail.com>
2024-10-27 23:12:43 +0200

    Docker "wanna-be" setup

--- src/main/java/group/project/EventPlanner/EventPlannerApplication.java
+++ src/main/java/group/project/EventPlanner/EventPlannerApplication.java
@@ -2,8 +2,9 @@ package group.project.EventPlanner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class EventPlannerApplication {
 
 	public static void main(String[] args) {
