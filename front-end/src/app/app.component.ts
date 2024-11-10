583b8473db5b0690915cd6cb9b1057a2975586a3
Diana <diana.cracea@stud.ubbcluj.ro>
2024-11-10 10:52:06 +0200

    firebase connection

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {FirebaseService} from './services/firebase.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,10 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'front-end';
+export class AppComponent implements OnInit {
+  constructor(private firebaseService: FirebaseService) {}
+
+  ngOnInit() {
+    console.log('AppComponent initialized');
+  }
 }
