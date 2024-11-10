a5252907d24f54ac16b07f74aec22ca466dff242
Robert <robert.burlacu03@gmail.com>
2024-11-10 23:02:22 +0200

    Added routes for page redirecting, CSS still not working properly

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,8 +1,17 @@
-import { Routes } from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import { EventFormComponent } from './components/event-form/event-form.component';
 import {LoginComponent} from './components/login/login.component';
+import {NgModule} from '@angular/core';
 
 export const routes: Routes = [
     { path: 'event-form', component: EventFormComponent },
-    { path: 'login', component: LoginComponent}
+    { path: 'login', component: LoginComponent },
+    { path: '', redirectTo: 'login', pathMatch: 'full'}
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
