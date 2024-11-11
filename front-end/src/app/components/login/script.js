0f47fc3853ea20a7bbd3d113827794fd737edcf6
Robert <robert.burlacu03@gmail.com>
2024-11-11 12:46:18 +0200

    Redone bits of login.component.html and script.js (login not working properly)

--- front-end/src/app/components/login/script.js
+++ front-end/src/app/components/login/script.js
@@ -1,3 +1,21 @@
+// import { initializeApp } from "firebase/auth";
+// import { getDatabase, ref, set, update } from "firebase/database";
+// import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth";
+//
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC5ftHFsiiB5VC71rre52EDHMVpvi_5aAY",
+//   authDomain: "mybigday-53567.firebaseapp.com",
+//   databaseURL: "https://mybigday-53567-default-rtdb.europe-west1.firebasedatabase.app",
+//   projectId: "mybigday-53567",
+//   storageBucket: "mybigday-53567.firebasestorage.app",
+//   messagingSenderId: "493056483296",
+//   appId: "1:493056483296:web:c57ddd659f13e91a821585"
+// };
+//
+// const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const database = getDatabase(app);
+
 document.getElementById("login-form").addEventListener("submit", function(event) {
   event.preventDefault();
 
@@ -6,13 +24,11 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   const errorMessage = document.getElementById("error-message");
 
   if (!validate_email(email)) {
-    alert("Authentication failed")
     errorMessage.textContent = "Adresa de email nu este valida.";
     return;
   }
 
   if (password.length < 6) {
-    alert("Authentication failed")
     errorMessage.textContent = "Parola trebuie sa aiba cel putin 6 caractere.";
     return;
   }
@@ -20,60 +36,25 @@ document.getElementById("login-form").addEventListener("submit", function(event)
   errorMessage.textContent = "";
 
   if (email === "test@gmail.com" && password === "parola123") {
-    alert("Autentificare reusita!");
-    window.location.href = "dashboard.html";
+    alert("Authentication successful!");
+    window.location.href = "./app/components/event-form/event-form.html";
   } else {
     errorMessage.textContent = "Incorrect email or password.";
   }
 });
 
 // document.addEventListener('DOMContentLoaded', function () {
-//   const register_form = document.getElementById('register-form');
+//  // const register_form = document.getElementById('register-form');
 //   const login_form = document.getElementById('login-form');
 //
-//   register_form.addEventListener('click', function() {
-//     register(auth);
-//   });
+//   //register_form.addEventListener('click', function() {
+//    //register(auth);
+//   //});
 //   login_form.addEventListener('click', function() {
 //     login(auth);
 //   });
 // });
-
-// function register(auth) {
-//   let first_name = document.getElementById("firstNameInp").value;
-//   let last_name = document.getElementById("lastNameInp").value
-//   let email = document.getElementById("email").value;
-//   let password = document.getElementById("password").value;
 //
-//   if (validate_email(email) === false || validate_password(password) === false) {
-//     alert("Invalid email address or password. Please try again.");
-//     return;
-//   }
-//   if(validate_field(first_name) === false || validate_field(last_name) === false || validate_field(email) === false || validate_field(password) === false) {
-//     alert("Please fill out all fields.");
-//     return;
-//   }
-//
-//   createUserWithEmailAndPassword(auth, email, password)
-//     .then(function (){
-//       let user = auth.currentUser;
-//       let user_data = {
-//         first_name: first_name,
-//         last_name: last_name,
-//         email: email,
-//         last_login: Date.now()
-//       }
-//       set(ref(database, 'users/' + user.uid), user_data)
-//
-//       alert('User created successfully!');
-//     })
-//     .catch(function(error) {
-//       let errorMessage = error.message;
-//       alert(errorMessage);
-//     });
-//
-// }
-
 // function login(auth){
 //   let email = document.getElementById("email").value;
 //   let password = document.getElementById("password").value;
@@ -101,11 +82,11 @@ document.getElementById("login-form").addEventListener("submit", function(event)
 //       alert(errorMessage);
 //     });
 // }
-
-function validate_email(email) {
-  let expression = /^[^@]+@\w+(\.\w+)+\w$/
-  return expression.test(email) === true;
-}
+//
+// function validate_email(email) {
+//   let expression = /^[^@]+@\w+(\.\w+)+\w$/
+//   return expression.test(email) === true;
+// }
 //
 // function validate_password(password) {
 //   return password.length >= 6;
