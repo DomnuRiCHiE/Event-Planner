67388a7693cabe742f5e86bd6ae35f6b81e3ca4c
IulianCosman <iuliancosman709@gmail.com>
2024-11-11 11:45:51 +0200

    Attendee events-list front-end done.

--- front-end/src/app/app.routes.ts
+++ front-end/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { EventFormComponent } from './components/event-form/event-form.component';
+import {EventsListComponent} from './components/events-list/events-list.component';
 
 export const routes: Routes = [
     { path: 'event-form', component: EventFormComponent },
+    { path: 'events-list', component: EventsListComponent}
 ];
