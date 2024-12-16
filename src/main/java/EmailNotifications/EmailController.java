0fa4bdf00a45e8d87a8af7b8e88309c3d82a0c67
Robert <robert.burlacu03@gmail.com>
2024-12-16 15:23:35 +0200

    Changed http configuration to using axios for front-end email sending (getting back-end error 404, server is running though)

--- src/main/java/EmailNotifications/EmailController.java
+++ src/main/java/EmailNotifications/EmailController.java
@@ -1,10 +1,12 @@
 package EmailNotifications;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+@AllArgsConstructor
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "http://localhost:4200")
